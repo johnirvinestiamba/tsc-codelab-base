e408825ae0e8bc6b2f581f6b173c631ccf76113a
John Irvin Estiamba <jestiamba@fullscale.io>
2019-06-25 09:45:41 +0800

    added get request for github user

--- script.ts
+++ script.ts
@@ -21,3 +21,25 @@ class Profile implements GitHubUser{
         this.hireable = hireable;
     }
 }
+
+function getGitHubUser(username: string, callback: any) {
+    let url = 'https://api.github.com/users/' + username;
+    const xhr = new XMLHttpRequest();
+ 
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            callback(xhr.response);
+        }
+    }
+    xhr.open('GET', url, true);
+    xhr.send('');
+}
+
+function searchUser() {
+    const username = (<HTMLInputElement>document.getElementById('username')).value;
+    getGitHubUser(username, (response: string) => {
+        let responseObj = JSON.parse(response);
+        console.log('responseObj : ', responseObj);
+    })
+
+}
